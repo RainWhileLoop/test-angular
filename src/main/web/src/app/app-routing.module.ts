ed945097a1b956d34d5e103a8882071aafa42bc9
Thanthathon Buagird <rainwhileloop@gmail.com>
2018-11-04 15:41:49 +0700

    add orders module and component, set routerLinkactive by css class

--- src/main/web/src/app/app-routing.module.ts
+++ src/main/web/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 const routes:Routes = [
-  {path:'dashboard', loadChildren:'./dashboard/dashboard.module#DashboardModule'}
+  {path:'dashboard', loadChildren:'./dashboard/dashboard.module#DashboardModule'},
+  {path:'orders', loadChildren:'./orders/orders.module#OrdersModule'}
 ];
 
 @NgModule({
--- src/main/web/src/app/side-bar/side-bar.component.ts
+++ src/main/web/src/app/side-bar/side-bar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
+  urlDashboard = 'dashboard';
   constructor() { }
 
   ngOnInit() {
